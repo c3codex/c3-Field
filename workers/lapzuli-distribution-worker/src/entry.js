f7310c5bffbfb448ce8e0d2ce0c5060e0e550f97
c3 Community Partners DAO, LLC <contribute2c3communitypartners@gmail.com>
2026-09-12 21:30:42 -0500

    Route c3 Community Partners Buffer through Lapzuli

--- workers/lapzuli-distribution-worker/src/entry.js
+++ workers/lapzuli-distribution-worker/src/entry.js
@@ -1,9 +1,13 @@
 import legacyWorker from "./index.js";
 import { handleBufferRequest } from "./buffer-adapter.js";
+import { handleC3BufferRequest } from "./buffer-c3-adapter.js";
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
+    if (url.pathname.startsWith("/buffer/c3/")) {
+      return handleC3BufferRequest(request, env, url.pathname);
+    }
     if (url.pathname.startsWith("/buffer/")) {
       return handleBufferRequest(request, env, url.pathname);
     }
